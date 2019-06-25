00ffce6be74ed672663ea23e837d9f602e49c38c
Marcin Barańczak <46418050+MarcinBaranczak@users.noreply.github.com>
2019-06-25 13:32:57 +0200

    bubble sort added

---
+++ Start.py
@@ -0,0 +1,28 @@
+import random
+
+
+# Generating numbers for sorting
+def data_set(count):
+    tab_u = []  # unsorted tab
+
+    for i in range(count):
+        tab_u.append(random.randint(0, 1000))
+
+    return tab_u
+
+
+# Bubble
+def bubble_sort(tab):
+    for i in range(len(tab) - 1):
+        for j in range(len(tab) - 1):
+            if tab[j] > tab[j + 1]:
+                temp = tab[j]
+                tab[j] = tab[j + 1]
+                tab[j + 1] = temp
+
+    return tab
+
+
+# Main
+tab_unsort = data_set(10)
+print(bubble_sort(tab_unsort))
